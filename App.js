d78d3ed4d3016bd1a54ad0fb78778682f61f3c91
MR. MADI <64649909+KUN-web@users.noreply.github.com>
2024-04-09 12:33:49 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Upcomingbirthdays from "./Components/Upcomingbirthdays";
+import Profile from "./Components/Profile";
+import Features from "./Components/Features";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Upcomingbirthdays />
+      <Features />
+      <Profile />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
